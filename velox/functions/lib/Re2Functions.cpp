f37f651cb9251abcb861e77316486c1681d8f506
gouyikui <gouyikui@baidu.com>
2023-09-22 19:59:32 +0800

    update
    Change-Id: I4e44f52fa5681134f0e9f46b2cf5da8b8295969a

--- velox/functions/lib/Re2Functions.cpp
+++ velox/functions/lib/Re2Functions.cpp
@@ -739,7 +739,8 @@ void re2SplitAll(
 
 class Re2SplitAllConstantPattern final : public VectorFunction {
  public:
-  explicit Re2SplitAllConstantPattern(RE2& re) : re_(re) {}
+  explicit Re2SplitAllConstantPattern(StringView pattern)
+      : re_(toStringPiece(pattern), RE2::Quiet) {}
 
   void apply(
       const SelectivityVector& rows,
@@ -776,7 +777,7 @@ class Re2SplitAllConstantPattern final : public VectorFunction {
   }
 
  private:
-  RE2& re_;
+  RE2 re_;
 };
 
 template <bool (*Fn)(StringView, const RE2&)>
@@ -1208,10 +1209,7 @@ std::shared_ptr<VectorFunction> makeRe2SplitAll(
 
   auto pattern = constantPattern->as<ConstantVector<StringView>>()->valueAt(0);
 
-  RE2 re(toStringPiece(pattern), RE2::Quiet);
-  checkForBadPattern(re);
-
-  return std::make_shared<Re2SplitAllConstantPattern>(re);
+  return std::make_shared<Re2SplitAllConstantPattern>(pattern);
 }
 
 std::vector<std::shared_ptr<exec::FunctionSignature>> re2SplitAllSignatures() {
